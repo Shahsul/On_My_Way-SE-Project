97fdd5b3d40e119357db52b76c5910616996db00
Shahsul <76909183+Shahsul@users.noreply.github.com>
2021-05-23 01:29:45 +0300

    Create constants.dart

---
+++ Final_source_codes/lib/constants/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  // this class to handle all strings for my app
+  static var api_link =
+      'http://onmywaye3-001-site1.btempurl.com/Controllers/api.asmxhttp://onmywaye3-001-site1.btempurl.com/Controllers/api.asmx/';
+}
